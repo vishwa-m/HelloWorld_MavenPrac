24e33cfe3d2b58bd744682348a099aa42ba30074
vishwa-m <manchalav@HCLT.CORP.HCL.IN>
2016-11-17 23:27:58 +0530

    Added two more test files

--- src/test/java/com/vnath/maven/hw/test/TestUnit2.java
+++ src/test/java/com/vnath/maven/hw/test/TestUnit2.java
@@ -1,13 +1,16 @@
 package com.vnath.maven.hw.test;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class TestUnit2 {
 	
+	@Test
 	public void unit2Test1(){
 		Assert.assertTrue(true);
 	}
 	
+	@Test
 	public void unit2Test2(){
 		Assert.assertTrue(true);
 	}
