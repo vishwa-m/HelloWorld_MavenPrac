03dd625bf7eafa77a75595cddf8c3cf2080616ff
vishwa-m <manchalav@HCLT.CORP.HCL.IN>
2016-11-17 14:25:07 +0530

    Initial commit in master for pushing the repository to remote

---
+++ src/main/java/com/vnath/maven/hw/MyHelloWorld.java
@@ -0,0 +1,13 @@
+package com.vnath.maven.hw;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyHelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/vnath/maven/hw/test/MyHelloWorldTest.java
@@ -0,0 +1,38 @@
+package com.vnath.maven.hw.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyHelloWorldTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MyHelloWorldTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MyHelloWorldTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
