45587290e174639d9d5bd7834179ace3d60b1748
vishwa-m <manchalav@HCLT.CORP.HCL.IN>
2016-11-17 22:37:18 +0530

    Updated Unit tests

--- src/test/java/com/vnath/maven/hw/test/MyHelloWorldTest.java
+++ src/test/java/com/vnath/maven/hw/test/MyHelloWorldTest.java
@@ -1,36 +1,42 @@
 package com.vnath.maven.hw.test;
 
-import junit.framework.Test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/*import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import junit.framework.TestSuite;*/
 
 /**
  * Unit test for simple App.
  */
 public class MyHelloWorldTest 
-    extends TestCase
-{
+    {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public MyHelloWorldTest( String testName )
+   /* public MyHelloWorldTest( String testName )
     {
         super( testName );
-    }
+    }*/
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+	/*public static TestSuite suite()
     {
         return new TestSuite( MyHelloWorldTest.class );
-    }
+    }*/
+	
+	
 
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp1()
     {
         assertTrue( true );
@@ -39,6 +45,7 @@ public class MyHelloWorldTest
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp2()
     {
     	assertTrue( true );
@@ -47,6 +54,7 @@ public class MyHelloWorldTest
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp3()
     {
     	assertTrue( true );
@@ -55,16 +63,18 @@ public class MyHelloWorldTest
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp4()
     {
-    	assertTrue( false );
+    	assertTrue( true );
     }
     
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp5()
     {
-    	assertTrue( false );
+    	assertTrue( true );
     }
 }
