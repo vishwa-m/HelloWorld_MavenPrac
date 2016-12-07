23becb8b3caf6bf194936b4bf6dc02d791a7db4a
vishwa-m <manchalav@HCLT.CORP.HCL.IN>
2016-12-07 18:30:39 +0530

    Modified Helloworld.java

--- src/main/java/com/vnath/maven/hw/MyHelloWorld.java
+++ src/main/java/com/vnath/maven/hw/MyHelloWorld.java
@@ -9,5 +9,16 @@ public class MyHelloWorld
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        MyHelloWorld mhw=new MyHelloWorld();
+        mhw.sum();
+    }
+    
+    private int sum(){
+    	int j=0;
+    	j=1+1;
+    	j=1+1;
+    	
+    	return j;
     }
 }
