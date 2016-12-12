2d4c41e1fc2af68d228a40a5eeb53b8727366ca7
vishwa-m <manchalav@LP-R9WLRZB.HCLT.CORP.HCL.IN>
2016-12-12 13:59:41 +0530

    Modified file

--- src/main/java/com/vnath/maven/hw/MyHelloWorld.java
+++ src/main/java/com/vnath/maven/hw/MyHelloWorld.java
@@ -1,24 +1,24 @@
 package com.vnath.maven.hw;
 
-/**
- * Hello world!
- *
- */
-public class MyHelloWorld 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        
-        MyHelloWorld mhw=new MyHelloWorld();
-        mhw.sum();
-    }
-    
-    private int sum(){
-    	int j=0;
-    	j=1+1;
-    	j=1+1;
-    	
-    	return j;
-    }
+public class MyHelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		int count = 0;
+		count=count+1;
+		
+		MyHelloWorld mhw = new MyHelloWorld();
+		mhw.sum();
+	}
+
+	private int sum() {
+		int j = 0;
+		j = 1 + 1;
+		return j;
+	}
+	
+	private int sum2() {
+		int j = 0;
+		j = 1 + 1;
+		return j;
+	}
 }
