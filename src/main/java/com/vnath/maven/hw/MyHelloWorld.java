71646250c1b66a1c21d5a8fda73b887cdf2134fc
vishwa-m <vishwanath.manchala@gmail.com>
2017-05-14 00:08:10 -0400

    Removed main method from MyHelloWorld.java

--- src/main/java/com/vnath/maven/hw/MyHelloWorld.java
+++ src/main/java/com/vnath/maven/hw/MyHelloWorld.java
@@ -1,13 +1,6 @@
 package com.vnath.maven.hw;
 
 public class MyHelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		int count = 0;
-		count = count + 1;
-
-		MyHelloWorld mhw = new MyHelloWorld();
-	}
 
 	public int sum(int i, int j) {
 		return i + j;
